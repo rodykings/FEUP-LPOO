539173e24ccb753243b06eab0e11eed4d9bf21ff
rodykings <rodrigocreis.12@gmail.com>
2020-03-01 22:47:39 +0000

    Progress in hero project

---
+++ hero/src/main/java/Application.java
@@ -0,0 +1,21 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Application {
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        Game game = new Game();
+        game.run();
+
+
+
+    }
+}
